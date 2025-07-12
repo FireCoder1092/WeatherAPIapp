debd979115f779ad8d85ffbe1e11b77fc0415731
FireCoder1092 <smaranpreethesh@gmail.com>
2025-07-12 15:13:20 +1200

    Day 31: Make table in Home page for station ID and station name

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
+stations = stations[["STAID", "STANAME"]]
 
 
-@app.route("/api/v1/<station>/<date>")
+@app.route("/api/v2/<station>/<date>")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
